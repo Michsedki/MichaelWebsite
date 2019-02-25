89b533f0428ba0a4547604b34fa79ab65da729fe
Apple <Michsedki@gmail.com>
2019-02-24 17:27:54 -0800

    test

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function Calculate() {
-    var billedAmount = document.getElementById("txtBilledAmount").value;
-    var percAmount = document.getElementById("txtPerc").value;
-    var tipAmount = billedAmount * (percAmount / 100);
-    alert(tipAmount)
-}
+
+<script src="https://www.gstatic.com/firebasejs/5.8.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCCH_S0KpnwMzqAG6EKRbpyAHPBZY_C-2A",
+    authDomain: "chcompetition.firebaseapp.com",
+    databaseURL: "https://chcompetition.firebaseio.com",
+    projectId: "chcompetition",
+    storageBucket: "chcompetition.appspot.com",
+    messagingSenderId: "371946324972"
+  };
+  firebase.initializeApp(config);
+</script>
